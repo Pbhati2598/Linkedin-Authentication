11e2c4a7b8fe6837fa4f20474c88f37ddaf491a0
wnspushpendra <pushpendra@webnsoftsolution.com>
2024-12-28 15:29:53 +0530

    code auth linkedin

---
+++ lib/main.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:linkedin_login/linkedin_login.dart';
+
+
+
+// @TODO IMPORTANT - you need to change variable values below
+// You need to add your own data from LinkedIn application
+// From: https://www.linkedin.com/developers/
+// Please read step 1 from this link https://developer.linkedin.com/docs/oauth2
+String redirectUrl = 'https://www.youtube.com/callback';
+String clientId = '776rnw4e4izlvg';
+String clientSecret = 'rQEgboUHMLcQi59v';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LinkedinLogin(),
+    );
+  }
+}
+
+class LinkedinLogin extends StatelessWidget {
+  const LinkedinLogin({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(final BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.person),
+                  text: 'Profile',
+                ),
+                Tab(icon: Icon(Icons.text_fields), text: 'Auth code'),
+              ],
+            ),
+            title: const Text('LinkedIn Authorization'),
+          ),
+          body: const TabBarView(
+            children: [
+              LinkedInProfileExamplePage(),
+              LinkedInAuthCodeExamplePage(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LinkedInProfileExamplePage extends StatefulWidget {
+  const LinkedInProfileExamplePage({super.key});
+
+  @override
+  State createState() => _LinkedInProfileExamplePageState();
+}
+
+class _LinkedInProfileExamplePageState
+    extends State<LinkedInProfileExamplePage> {
+  UserObject? user;
+  bool logoutUser = false;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          LinkedInButtonStandardWidget(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (final BuildContext context) => LinkedInUserWidget(
+                    appBar: AppBar(
+                      title: const Text('OAuth User'),
+                    ),
+                    destroySession: logoutUser,
+                    redirectUrl: redirectUrl,
+                    clientId: clientId,
+                    clientSecret: clientSecret,
+                    onError: (final UserFailedAction e) {
+                      print('Error: ${e.toString()}');
+                      print('Error: ${e.stackTrace.toString()}');
+                    },
+                    onGetUserProfile: (final UserSucceededAction linkedInUser) {
+                      print(
+                        'Access token ${linkedInUser.user.token}',
+                      );
+
+                      print('User sub: ${linkedInUser.user}');
+
+                      user = UserObject(
+                        firstName: linkedInUser.user.localizedFirstName,
+                        lastName: linkedInUser.user.localizedLastName,
+                        email: linkedInUser.user.email,
+                        profileImageUrl: linkedInUser.user.profilePicture,
+                      );
+
+                      setState(() {
+                        logoutUser = false;
+                      });
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+          ),
+          LinkedInButtonStandardWidget(
+            onTap: () {
+              setState(() {
+                user = null;
+                logoutUser = true;
+              });
+            },
+            buttonText: 'Logout',
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('First: ${user?.firstName} '),
+              Text('Last: ${user?.lastName} '),
+              Text('Email: ${user?.email}'),
+              Text('Profile image: ${user?.profileImageUrl}'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LinkedInAuthCodeExamplePage extends StatefulWidget {
+  const LinkedInAuthCodeExamplePage({super.key});
+
+  @override
+  State createState() => _LinkedInAuthCodeExamplePageState();
+}
+
+class _LinkedInAuthCodeExamplePageState
+    extends State<LinkedInAuthCodeExamplePage> {
+  AuthCodeObject? authorizationCode;
+  bool logoutUser = false;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        LinkedInButtonStandardWidget(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (final BuildContext context) => LinkedInAuthCodeWidget(
+                  destroySession: logoutUser,
+                  redirectUrl: redirectUrl,
+                  clientId: clientId,
+                  onError: (final AuthorizationFailedAction e) {
+                    print('Error: ${e.toString()}');
+                    print('Error: ${e.stackTrace.toString()}');
+                  },
+                  onGetAuthCode: (final AuthorizationSucceededAction response) {
+                    print('Auth code ${response.codeResponse.code}');
+
+                    print('State: ${response.codeResponse.state}');
+
+                    authorizationCode = AuthCodeObject(
+                      code: response.codeResponse.code,
+                      state: response.codeResponse.state,
+                    );
+                    setState(() {});
+
+                    Navigator.pop(context);
+                  },
+                ),
+                fullscreenDialog: true,
+              ),
+            );
+          },
+        ),
+        LinkedInButtonStandardWidget(
+          onTap: () {
+            setState(() {
+              authorizationCode = null;
+              logoutUser = true;
+            });
+          },
+          buttonText: 'Logout user',
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Auth code: ${authorizationCode?.code} '),
+              Text('State: ${authorizationCode?.state} '),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AuthCodeObject {
+  AuthCodeObject({required this.code, required this.state});
+
+  final String? code;
+  final String? state;
+}
+
+class UserObject {
+  UserObject({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.profileImageUrl,
+  });
+
+  final String? firstName;
+  final String? lastName;
+  final LinkedInProfileEmail? email;
+  final LinkedInProfilePicture? profileImageUrl;
+}
